d6c615c07c05d80f002f1205a06db894336a00cc
Brian Heise <bnheise@gmail.com>
2020-06-03 09:34:00 +0900

    built a basic sap fiori product display app

---
+++ webapp/controller/ProductDetail.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+  "tutorial/products/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("tutorial.products.controller.ProductDetail", {
+
+    onInit: function () {
+      const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      oRouter.getRoute("ProductDetail").attachMatched(this._onRouteMatched, this);
+    },
+
+    _onRouteMatched: function (oEvent) {
+      const iProductId = oEvent.getParameter("arguments").productId;
+      const oView = this.getView();
+      oView.bindElement({
+        path: "/Products(" + iProductId + ")",
+        events: {
+          dataRequested: function () {
+            oView.setBusy(true);
+          },
+          dataReceived: function () {
+            oView.setBusy(false);
+          }
+        }
+      });
+    },
+
+  });
+});
